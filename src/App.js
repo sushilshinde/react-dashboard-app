d76d08d6e25417777fc60cc2f7ccab52ea334e9d
Sushil <shinde.sushil@gmail.com>
2019-03-03 11:54:34 +0530

    deploy on heroku

--- src/App.js
+++ src/App.js
@@ -2,25 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Dashboard from './Dashboard';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. asdjkaldjalsdjlajdlajs
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Dashboard></Dashboard>
     );
   }
 }
---
+++ src/SimpleLineChart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const data = [
+  { name: 'Mon', Visits: 2200, Orders: 3400 },
+  { name: 'Tue', Visits: 1280, Orders: 2398 },
+  { name: 'Wed', Visits: 5000, Orders: 4300 },
+  { name: 'Thu', Visits: 4780, Orders: 2908 },
+  { name: 'Fri', Visits: 5890, Orders: 4800 },
+  { name: 'Sat', Visits: 4390, Orders: 3800 },
+  { name: 'Sun', Visits: 4490, Orders: 4300 },
+];
+
+function SimpleLineChart() {
+  return (
+    // 99% per https://github.com/recharts/recharts/issues/172
+    <div></div>
+  );
+}
+
+export default SimpleLineChart;
